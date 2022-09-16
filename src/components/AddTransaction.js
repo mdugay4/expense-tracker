292576df2842fec56a5ac19b6fbdffedfbcbdae8
Mark Dugay <mdugay4@gmail.com>
2022-09-15 19:16:47 -0700

    Global State and Transaction History

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+export const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
+    return (
+        <>
+            <h3>Add new transaction</h3>
+            <form>
+                <div className='form-control'>
+                    <label htmlFor='text'>Text</label>
+                    <input
+                        type='text'
+                        value={text}
+                        onChange={() => setText(e.target.value)}
+                        placeholder='Enter text...'
+                    />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='amount'>
+                        Amount <br />
+                        (negative - expense, positive - income)
+                    </label>
+                    <input
+                        type='number'
+                        value={amount}
+                        onChange={() => setAmount(e.target.value)}
+                        placeholder='Enter amount...'
+                    />
+                </div>
+                <button className='btn'>Add transaction</button>
+            </form>
+        </>
+    );
+};
