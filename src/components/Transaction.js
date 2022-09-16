aeb5ee8b7eefecdb4cac22575e8729606b1cb1c5
Mark Dugay <mdugay4@gmail.com>
2022-09-15 19:29:29 -0700

    Transaction Deletion

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+
     // Get sign
     const sign = transaction.amount < 0 ? '-' : '+';
 
@@ -10,7 +14,12 @@ export const Transaction = ({ transaction }) => {
             <span>
                 {sign}${Math.abs(transaction.amount)}
             </span>
-            <button className='delete-btn'>x</button>
+            <button
+                onClick={() => deleteTransaction(transaction.id)}
+                className='delete-btn'
+            >
+                x
+            </button>
         </li>
     );
 };
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -2,5 +2,12 @@ export default (state, action) => {
     switch (action.type) {
         default:
             return state;
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(
+                    (transaction) => transaction.id !== action.payload
+                ),
+            };
     }
 };
